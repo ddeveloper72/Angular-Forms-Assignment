1a492bb26514015e679fc7d5240686f019c2b1e0
ddeveloper72 <duncan.niamh@gmail.com>
2019-07-27 21:28:40 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
