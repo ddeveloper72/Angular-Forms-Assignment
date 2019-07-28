763f5ee6599f8a1cedb26abdde51f901efba1eda
ddeveloper72 <duncan.niamh@gmail.com>
2019-07-28 12:17:48 +0100

    Added subscription services and onSubmit method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+  subServices = ['Basic', 'Advanced', 'Pro'];
+  defaultService = 'Advanced';
+  @ViewChild('subscriptionForm', {static: false}) sgnForm: NgForm;
+
+  onSubmit() {
+    console.log(this.sgnForm.value);
+    }
 }
